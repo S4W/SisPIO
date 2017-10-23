3ea794764f684d9126731985d6c7c9016ad399e3
Miguel Canedo <mi.canedo10@gmail.com>
2017-10-23 19:12:30 -0400

    Arregladas regex e incluido correo en tablas

--- models/db.py
+++ models/db.py
@@ -156,10 +156,11 @@ db.define_table(
 
 db.define_table(
     'estudiante',
-    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1, 8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1,8}$', error_message='Numero de Cedula Invalido.')]),
     Field('promedio', type='double', notnull=True, requires=IS_FLOAT_IN_RANGE(minimum=0.01,maximum=20, error_message='Promedio no valido.')),
+    Field('correo', type='string', length=128, required=True, default='', requires=IS_EMPTY_OR(IS_EMAIL(error_message='Debe tener un formato válido. EJ: example@org.com'))),
     Field('direccion', type='text', default=''),
-    Field('telefono_habitacion', type ='string', length=11, requires=IS_MATCH('^((0)?2[0-9]{2}(-)?)?[0-9]{7}$', error_message='Telefono Habitación Invalido.')),
+    Field('telefono_habitacion', type ='string', length=12, requires=IS_MATCH('^((0)?2[0-9]{2}(-)?)?[0-9]{7}$', error_message='Telefono Habitación Invalido.')),
     Field('telefono_otro', type ='string', length=11, requires=IS_MATCH('^((0)?[0-9]{3}(-)?)?[0-9]{7}$', error_message='Telefono Invalido.')),
     Field('fecha_nacimiento', type='date', requires=IS_EMPTY_OR(IS_DATE(format=T('%d/%m/%Y'), error_message='Debe ser del siguiente formato: dd/mm/yyyy'))),
     Field('sexo', type='string', requires=IS_IN_SET(['Masculino', 'Femenino'])),
@@ -167,14 +168,14 @@ db.define_table(
     Field('estatus', type='string', default='Pre-inscrito', requires=IS_IN_SET(['Pre-inscrito', 'Seleccionado', 'Activo', 'Inactivo', 'Finalizado'])),
     Field('cohorte', type='string', default='', requires=IS_IN_DB(db, db.cohorte.identificador)),
 
-    Field('ci_representante', type='string', length=8, default='', requires=IS_MATCH('^[0-9]{1, 8}$', error_message='Numero de Cedula Invalido.')),
+    Field('ci_representante', type='string', length=8, default='', requires=IS_MATCH('^[0-9]{1,8}$', error_message='Numero de Cedula Invalido.')),
     Field('nombre_representante', type='string', default=''),
     Field('apellido_representante', type='string', default=''),
     Field('sexo_representante', type='string', requires=IS_IN_SET(['Masculino', 'Femenino'])),
     Field('correo_representante', type='string', length=128, required=True, default='', requires=IS_EMPTY_OR(IS_EMAIL(error_message='Debe tener un formato válido. EJ: example@org.com'))),
     Field('direccion_representante', type='text', default=''),
-    Field('telefono_representante_oficina', type ='integer', length=11, requires=IS_MATCH('^((0)?2[0-9]{2}(-)?)?[0-9]{7}$', error_message='Telefono Oficina Invalido.')),
-    Field('telefono_representante_otro', type ='integer', length=11, requires=IS_MATCH('^((0)?[0-9]{3}(-)?)?[0-9]{7}$', error_message='Telefono Invalido.')),
+    Field('telefono_representante_oficina', type ='integer', length=12, requires=IS_MATCH('^((0)?2[0-9]{2}(-)?)?[0-9]{7}$', error_message='Telefono Oficina Invalido.')),
+    Field('telefono_representante_otro', type ='integer', length=12, requires=IS_MATCH('^((0)?[0-9]{3}(-)?)?[0-9]{7}$', error_message='Telefono Invalido.')),
 
     Field('sufre_enfermedad', type='boolean', default=False),
     Field('enfermedad', type='string'),
@@ -185,22 +186,25 @@ db.define_table(
 
 db.define_table(
     'profesor',
-    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1, 8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1,8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('correo', type='string', length=128, required=True, default='', requires=IS_EMPTY_OR(IS_EMAIL(error_message='Debe tener un formato válido. EJ: example@org.com'))),
 
     migrate="db.profesor"
     )
 
 db.define_table(
     'representante_sede',
-    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1, 8}$', error_message='Numero de Cedula Invalido.')]),
-    Field('sede', 'string'),
+    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1,8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('correo', type='string', length=128, required=True, default='', requires=IS_EMPTY_OR(IS_EMAIL(error_message='Debe tener un formato válido. EJ: example@org.com'))),
+    Field('sede', type='string', requires=IS_IN_SET(['Sartenejas', 'Litoral', 'Higuerote', 'Guarenas'])),
 
     migrate="db.representante_sede"
     )
 
 db.define_table(
     'representante_liceo',
-    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1, 8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('ci', type='string', length=8, notnull=True, unique=True, requires=[IS_IN_DB(db, db.usuario.username), IS_MATCH('^[0-9]{1,8}$', error_message='Numero de Cedula Invalido.')]),
+    Field('correo', type='string', length=128, required=True, default='', requires=IS_EMPTY_OR(IS_EMAIL(error_message='Debe tener un formato válido. EJ: example@org.com'))),
     Field('nombre_liceo', type='string', required=True, requires=IS_IN_DB(db, db.liceo.nombre)),
 
     migrate="db.representante_liceo"
@@ -244,7 +248,7 @@ db.define_table(
     'exime',
     Field('ci_estudiante', type='string', length=8, notnull=True, requires=IS_IN_DB(db, db.estudiante.ci)),
     Field('ci_representante_liceo', type='string', length=8, notnull=True, requires=IS_IN_DB(db, db.representante_liceo.ci)),
-    Field('cohorte', type='string', notnull=True),
+    Field('cohorte', type='string', notnull=True, requires=IS_IN_DB(db, db.cohorte.identificador)),
 
     migrate='db.exime'
     )
